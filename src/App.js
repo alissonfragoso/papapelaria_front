bc180b769841f849e67db3db4628f74f51baa107
alisson <130512076+alissonfragoso@users.noreply.github.com>
2023-11-20 20:28:29 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Logon from './pages/logon'
+
+function App() {
+  return(
+ <Logon />
+  )
+}
+
+export default App;
---
+++ src/pages/logon/index.js
@@ -0,0 +1,20 @@
+import './styles.css'
+
+
+export default function Logon(){
+   return(
+    <div className="logon-container">
+        <section className="form">
+            <h1>Faça seu login</h1>
+            <form >
+                <input placeholder="Email" />
+                <input placeholder="Senha" type='password' />
+                <button type="submit">Entrar</button>
+                <a href="#">Novo Cadastro</a>
+            </form>
+        </section>
+    </div>
+
+   )
+
+}
